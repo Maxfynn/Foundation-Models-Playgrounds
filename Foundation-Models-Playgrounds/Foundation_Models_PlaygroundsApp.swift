de85a43181ffe48754013388a2cc85caa077c825
Ivan Campos <sopmac21379@gmail.com>
2025-06-11 15:18:48 -0400

    Initial Commit

---
+++ Foundation-Models-Playgrounds/Foundation_Models_PlaygroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foundation_Models_PlaygroundsApp.swift
+//  Foundation-Models-Playgrounds
+//
+//  Created by IVAN CAMPOS on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Foundation_Models_PlaygroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
